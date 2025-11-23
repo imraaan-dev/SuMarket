32f7067634bb10521a5cc545ffbd85621c5f4076
aminAzaka2 <amin.zakaurrehman@sabanciuniv.edu>
2025-11-24 00:05:12 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,93 @@
 import 'package:flutter/material.dart';
-import 'screens/homePage.dart';
+
+import 'screens/auth/login_screen.dart';
+import 'screens/auth/signup_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => const HomePage(),
-      '/login': (context) => const HomePage(),
-      '/signup': (context) => const HomePage(),
-      '/create-post': (context) => const HomePage(),
-      '/favorites': (context) => const HomePage(),
-      '/notifications': (context) => const HomePage(),
-      '/profile': (context) => const HomePage(),
-      '/viewItem': (context) => const HomePage(),
-      '/all-messages': (context) => const HomePage(),
+  runApp(const SuMarketApp());
+}
 
+class SuMarketApp extends StatelessWidget {
+  const SuMarketApp({super.key});
 
-    },
-  ));
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SU Market',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1E88E5),
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        LoginScreen.routeName: (_) => const LoginScreen(),
+        SignUpScreen.routeName: (_) => const SignUpScreen(),
+        MainNavigation.routeName: (_) => const MainNavigation(),
+      },
+    );
+  }
+}
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  static const routeName = '/main';
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    _PlaceholderScreen(), // Placeholder until teammates add screens
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (index) {
+          setState(() => _currentIndex = index);
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.more_horiz),
+            selectedIcon: Icon(Icons.more_horiz),
+            label: 'More',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PlaceholderScreen extends StatelessWidget {
+  const _PlaceholderScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('More screens coming soon'),
+      ),
+    );
+  }
 }
